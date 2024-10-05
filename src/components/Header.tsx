fe59a1d5f383a1747035e6991578ce9f1ec37c9e
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-10-05 02:40:37 +0100

    add app logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
+import AppLogo from "./icons/AppLogo";
+
 const AppHeader = () => {
-  return <header className="h-48 bg-base-gray-700"></header>;
+  return (
+    <header className="flex justify-center items-center h-48 bg-base-gray-700">
+      <AppLogo />
+    </header>
+  );
 };
 
 export default AppHeader;
